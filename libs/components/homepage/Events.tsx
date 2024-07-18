db859b2ea004c3996a40987194867c21c5d8d0ed
javokhirMuminov <javokhirmuminovb@gmail.com>
2024-07-18 19:25:27 +0900

    add popular agent

--- libs/components/homepage/Events.tsx
+++ libs/components/homepage/Events.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Stack, Box } from '@mui/material';
+import { Stack, Box, Link, Button } from '@mui/material';
 import useDeviceDetect from '../../hooks/useDeviceDetect';
+import { T } from '../../types/common';
 
 interface EventData {
 	eventTitle: string;
@@ -8,7 +9,12 @@ interface EventData {
 	description: string;
 	imageSrc: string;
 }
+
+
+
+
 const eventsData: EventData[] = [
+
 	{
 		eventTitle: 'Paradise City Theme Park',
 		city: 'Incheon',
@@ -33,7 +39,7 @@ const eventsData: EventData[] = [
 		city: 'Busan',
 		description:
 			'Haeundae Sand Festival, the nation’s largest eco-friendly exhibition on sand, is held at Haeundae Beach!',
-		imageSrc: '/img/events/BUSAN.webp',
+		imageSrc: '',
 	},
 ];
 
@@ -72,19 +78,136 @@ const Events = () => {
 		return <div>EVENT CARD</div>;
 	} else {
 		return (
-			<Stack className={'events'}>
+			<Stack className={'main-content'}>
 				<Stack className={'container'}>
-					<Stack className={'info-box'}>
-						<Box component={'div'} className={'left'}>
-							<span className={'white'}>Events</span>
-							<p className={'white'}>Events waiting your attention!</p>
-						</Box>
-					</Stack>
-					<Stack className={'card-wrapper'}>
-						{eventsData.map((event: EventData) => {
-							return <EventCard event={event} key={event?.eventTitle} />;
-						})}
-					</Stack>
+					<div className={'leaut-2-1'}>
+
+				<div className={'layout-2-1-main'}>
+				 <section className={'global-news'}>
+					<h2 className={'global-news-heading'}>
+
+
+						{/* <!-- NEWS-LIST --> */}
+						<div className={'new-list'}>
+							<div className={'news-list-item article-card-horizontal'}>
+								<img className={'article-card-horizontal-img'} src="/img/banner/audi.jpg" alt=""  width="310" height="165"/>
+								<div className={'article-card-horizontal-content'}>
+						  		<h3 className={'article-card-horizontal-title'}>
+										Отопление Ташкента на 30 лет переходит в управление французской Veolia
+									</h3>
+									<p className={'article-card-horizontal-text'}>Система теплоснабжения Ташкента на 30 лет будет передана в управление французской компании Veolia для модернизации и реконструкции, сообщает 22 сентября пресс-служба...</p>
+									<footer className={'article-card-footer'}>
+										<img className={'artical-card-source-icon'} src="/img/logo/audi-logo.jpg" alt=""  width="20" height="20"/>
+										<div className={'article-card-time'}>
+											<span className={'article-card-source'}>ИА Красная Весна 15:26</span>
+										</div>
+									</footer>
+								</div>
+							</div>
+						</div>
+
+
+						<div className={'new-list'}>
+							<div className={'news-list-item article-card-horizontal'}>
+								<img className={'article-card-horizontal-img'} src="/img/banner/codilac.jpg" alt=""  width="100%" height="165"/>
+								<div className={'article-card-horizontal-content'}>
+							   	<h3 className={'article-card-horizontal-title'}>
+										Отопление Ташкента на 30 лет переходит в управление французской Veolia
+									</h3>
+									<p className={'article-card-horizontal-text'}>Система теплоснабжения Ташкента на 30 лет будет передана в управление французской компании Veolia для модернизации и реконструкции, сообщает 22 сентября пресс-служба...</p>
+									<footer className={'article-card-footer'}>
+										<img className={'artical-card-source-icon'} src="/img/logo/codilac-logo.jpg" alt=""  width="20" height="20"/>
+										<div className={'article-card-time'}>
+											<span className={'article-card-source'}>ИА Красная Весна 15:26</span>
+										</div>
+									</footer>
+								</div>
+							</div>
+						</div>
+
+						<div className={'new-list'}>
+							<div className={'news-list-item article-card-horizontal'}>
+								<img className={'article-card-horizontal-img'} src="/img/banner/bmw.webp" alt=""  width="310" height="265"/>
+								<div className={'article-card-horizontal-content'} >
+									<h3 className={'article-card-horizontal-title'}>
+										Отопление Ташкента на 30 лет переходит в управление французской Veolia
+									</h3>
+									<p className={'article-card-horizontal-text'}>Система теплоснабжения Ташкента на 30 лет будет передана в управление французской компании Veolia для модернизации и реконструкции, сообщает 22 сентября пресс-служба...</p>
+									<footer className={'article-card-footer'}>
+										<img className={'artical-card-source-icon'} src="/img/logo/bmw-logo.png" alt=""  width="20" height="20"/>
+										<div className={'article-card-time'}>
+											<span className={'article-card-source'}>ИА Красная Весна 15:26</span>
+										</div>
+									</footer>
+								</div>
+							</div>
+						</div>
+
+						<div className={'new-list'}>
+							<div className={'news-list-item article-card-horizontal'}>
+								<img className={'article-card-horizontal-img'} src="/img/events/BUSAN.webp" alt=""  width="210" height="165"/>
+								<div className={'article-card-horizontal-content'}>
+						  		<h3 className={'article-card-horizontal-title'}>
+										Отопление Ташкента на 30 лет переходит в управление французской Veolia
+									</h3>
+									<p className={'article-card-horizontal-text'}>Система теплоснабжения Ташкента на 30 лет будет передана в управление французской компании Veolia для модернизации и реконструкции, сообщает 22 сентября пресс-служба...</p>
+									<footer className={'article-card-footer'}>
+
+										<img className={'artical-card-source-icon'} src="/img/logo/mers-logo.jpg" alt=""  width="20" height="20"/>
+										<div className={'article-card-time'}>
+											<span className={'article-card-source'}>ИА Красная Весна 15:26</span>
+										</div>
+									</footer>
+								</div>
+							</div>
+						</div>
+					</h2>
+				 </section>
+				 </div>
+
+
+				{/* LAYOUT 2-1 SAIDBAR */}
+
+				<div className={'layout-2-1-saidbar'}>
+
+
+
+					<aside className={'layout-2-1-sidebar-item sidebar-ad-link'}>
+					<video
+					autoPlay
+					muted
+					loop
+					playsInline
+					preload="auto"
+					style={{ width: '393px', height: '500px', objectFit: 'cover' }}
+				>
+					<source src="/video/ads5.mp4" type="video/mp4" />
+				</video>
+					</aside>
+
+					<aside className={'layout-2-1-sidebar-item '}>
+						<a className={' sidebar-ad-img2'} href="#">
+						<video
+					autoPlay
+					muted
+					loop
+					playsInline
+					preload="auto"
+					style={{ width: '393px', height: '500px', objectFit: 'cover' }}
+				>
+					<source src="/video/asd6.mp4" type="video/mp4" />
+				</video>
+							{/* <img  className={"sidebar-ad-img2"} src="/img/events/BUSAN.webp" alt=""  width="393" height="500"  /> */}
+						</a>
+					</aside>
+				</div>
+
+
+
+				 </div>
+
+
+
 				</Stack>
 			</Stack>
 		);
